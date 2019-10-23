35c48038f9fc4751cd278329849984d2d17f7734
jcesarmobile <jcesarmobile@gmail.com>
2019-10-23 16:05:21 +0200

    Initial Commit

---
+++ background-audio-bug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  background-audio-bug
+//
+//  Created by Julio Cesar Sanchez Hernandez on 23/10/2019.
+//  Copyright © 2019 Julio Cesar Sanchez Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
