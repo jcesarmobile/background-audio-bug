f5a8016c032531cae6e9d41b86c82dff2af8033b
jcesarmobile <jcesarmobile@gmail.com>
2019-10-23 16:09:38 +0200

    basic functionality

--- background-audio-bug/ViewController.swift
+++ background-audio-bug/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+class ViewController: UIViewController, WKNavigationDelegate {
+var webView: WKWebView!
+override func viewDidLoad() {
+super.viewDidLoad()
+// Do any additional setup after loading the view.
+let html = """
+<br><br><br>
+<audio controls="controls">
+<source src="https://start-sud.ice.infomaniak.ch/start-sud-high.mp3" type="audio/mp3" />
+</audio>
+"""
+webView.loadHTMLString(html, baseURL: nil)
+webView.allowsBackForwardNavigationGestures = true
 
 }
+override func loadView() {
+webView = WKWebView()
+webView.navigationDelegate = self
+view = webView
+}
+} 
 
